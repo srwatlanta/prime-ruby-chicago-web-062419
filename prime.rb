74e5a09d826f5c2d9bdb71871899aba8b62a33a7
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-01 00:53:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,10 @@ def prime? (n)
   if n <= 1
     return false
   else
-  (2..n).each do |i|
-  n % i == 0
-  return false
+  (2..n - 1).each do |i|
+  return false if n % i == 0
+end
+true
 end
  end
 end
